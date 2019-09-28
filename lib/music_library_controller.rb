4f476bfed5040d6a5493bd883c84bcdba801aad5
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-28 01:58:58 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -9,42 +9,42 @@ class MusicLibraryController
    music.import 
   end 
   
-  def call 
+  # def call 
     
-    response = gets.chomp  
-    until response == exit
+  #   response = gets.chomp  
+  #   until response == exit
       
-  # if response != "exit"
+  # # if response != "exit"
     
-    puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
+  #   puts "Welcome to your music library!"
+  #   puts "To list all of your songs, enter 'list songs'."
+  #   puts "To list all of the artists in your library, enter 'list artists'."
+  #   puts "To list all of the genres in your library, enter 'list genres'."
+  #   puts "To list all of the songs by a particular artist, enter 'list artist'."
+  #   puts "To list all of the songs of a particular genre, enter 'list genre'."
+  #   puts "To play a song, enter 'play song'."
+  #   puts "To quit, type 'exit'."
+  #   puts "What would you like to do?"
   
-    case response 
-      when 'list songs'
-        list_songs
-      when 'list artists'
-        list_artists
-      when 'list genres'
-        list_genres
-      when 'list artist'
-        list_songs_by_artist
-      when 'list genre'
-        list_songs_by_genre
-      when 'play song'
-        play_song
-      else 
-        "What would you like to do?"
-      end 
-    end 
+  #   case response 
+  #     when 'list songs'
+  #       list_songs
+  #     when 'list artists'
+  #       list_artists
+  #     when 'list genres'
+  #       list_genres
+  #     when 'list artist'
+  #       list_songs_by_artist
+  #     when 'list genre'
+  #       list_songs_by_genre
+  #     when 'play song'
+  #       play_song
+  #     else 
+  #       "What would you like to do?"
+  #     end 
+  #   end 
 
-  end 
+  # end 
     
   
   def list_songs
